fa391e2e8bf83f5daea3080dbd42c0665eed53a9
Noric Couderc <noric.couderc@cs.lth.se>
2022-03-07 20:36:36 +0100

    Restructured presentation
    Made some combinators for the picts for my slides (works much better!)
    
    Added some stuff about the related work!

--- presentation/presentation-gradual-typing.rkt
+++ presentation/presentation-gradual-typing.rkt
@@ -76,29 +76,267 @@ latex
                                             (latex exp)) " ")]
     [s s]))
 
-(define (pict exp)
+;; Size of a space in LaTeX
+(define ltx-space
+  128/9)
+
+(define (ltx-concat . args)
+  (apply
+   (curry hbl-append ltx-space)
+   args))
+              
+  
+
+(define (dyn-type)
+  ($ (latex '(dyn-type))))
+
+(define (type t)
+  ($ (latex `(type ,t))))
+
+(define (subtype t1 t2)
+  (hbl-append ltx-space t1 ($ "<:") t2))
+
+(define (type-var v)
+  ($ (latex `(type-var ,v))))
+
+(define (consistent x y)
+  (ltx-concat x ($ (latex-symbol "sim")) y))
+
+(define (inconsistent x y)
+  (ltx-concat x ($ (latex-symbol "nsim")) y))
+
+(define (has-type e t)
+  (ltx-concat e ($ ":") t))
+
+(define (fun-type it ot)
+  (ltx-concat it ($ "\\rightarrow") ot))
+
+(define (method name it ot)
+  (has-type ($ name)
+            (fun-type it ot)))
+
+
+(define (list-join c list)
+  (if (= (length list) 1)
+      list
+      (cons
+       (car list)
+       (cons
+        c
+        (list-join c (cdr list))))))          
+  
+(define (object . methods)
+  (apply ltx-concat
+         (append (list ($ "["))
+                 (list-join ($ ",") methods)
+                 (list ($ "]")))))
+
+
+(define (fast-pict exp)
   (match exp
     [`(rule ,premisse ,consequence)
-     (let [(p (pict premisse))]
+     (let [(p (fast-pict premisse))]
        (vc-append
         p
         (hline (pict-width p) 10)
-        (pict consequence)))]
-    [`(and ,p1 ,p2) (hb-append 50.0 (pict p1) (pict p2))]
+        (fast-pict consequence)))]
+    [`(and ,p1 ,p2) (hb-append 50.0 (fast-pict p1) (fast-pict p2))]
     [`(implies env ,cons) ($ (latex `(implies env ,cons)))]
     [`(inconsistent ,x ,y) ($ (latex `(inconsistent ,x ,y)))]
     [`(consistent ,x ,y) ($ (latex `(consistent ,x ,y)))]
     [`(type-var ,t) ($ (latex `(type-var ,t)))]
     [`(subtype ,t1 ,t2) ($ (latex `(subtype ,t1 ,t2)))]
     [`(type ,(? string? t)) ($ (latex `(type ,t)))]
-    [`(dyn-type) ($ (latex '(dyn-type)))]
+    [`(dyn-type) (dyn-type) ]
     ))
 
 ;; PRESENTATION
+
+(current-main-font "Lato Medium")
+
+(slide
+ (bt "Gradual typing for Objects")
+ (t "Noric Couderc"))
+
+(slide
+ #:title "The Problem"
+ 'next
+ (item (bt "Static typing"))
+ (subitem "full coverage")
+ (subitem "efficient execution")
+ (subitem "machine-checked documentation")
+ 'next
+ (item (bt "Dynamic typing"))
+ (subitem "more flexibility")
+ )
+
+(slide
+ (bt "Why not have both?"))
+
+(slide
+ (bt "What you want")
+ 'next
+ (item "Omit type annotations")
+ (subitem "Run program whenever you want")
+ 'next
+ (item "Add type annotation when you need")
+ (subitem "Fully annotated program = static type checking"))
+
+(slide
+ #:title "Alternatives"
+ (item "Type inference")
+ 'next
+ (subitem "Must typecheck before run")
+ 'next
+ (item "Type annotations")
+ 'next
+ (subitem "No proof of typing prevents all type errors")
+ 'next
+ (item "Soft typing / static analysis")
+ 'next
+ (subitem "Only warnings")
+ )
+
+(slide
+ #:title "The idea"
+ (item "add a type \"dynamic\":" (fast-pict '(dyn-type)))
+ (item "add relations between types and" (fast-pict '(dyn-type))))
+
+
+(slide
+ (item (fast-pict '(consistent (type "int") (type "int"))))
+ (item (fast-pict '(consistent (type "int") (dyn-type))))
+ (item ($ (latex '(consistent
+                   (fun-type (type "int") (dyn-type))
+                   (fun-type (dyn-type) (type "int")))))))
+
+
+(slide
+ ($ (latex '(consistent
+             (object
+              (method "x"
+                      (signature (type "int") (dyn-type)))
+              (method "y"
+                      (signature (dyn-type) (type "bool"))))
+             (object
+              (method "y"
+                      (signature (type "bool") (dyn-type)))
+              (method "x"
+                      (signature (dyn-type) (type "int"))))))))
+
+(slide
+ (inconsistent
+  (object
+   (method "x"
+           (type "int")
+           (type "int"))
+   (colorize
+    (method "y"
+            (dyn-type) (dyn-type))
+    "red"))
+  (object
+   (method "x"
+           (type "int") (type "int")))))
+
+
+(define inconsistent-example
+  (let* [(note (t "different types"))
+         (spacing 200)
+         (x (method "x" (type "int") (type "int")))
+         (x1 (method "x" (type "bool") (type "int")))
+         (y (method "y" (dyn-type) (type "bool")))]
+    ((compose
+      (lambda (p)
+        (linewidth 3 p))
+      (lambda (p)
+        (pin-arrow-line
+         10
+         ; Under
+         p
+         note
+         ct-find
+         x
+         cb-find))
+      (lambda (p)
+        (pin-arrow-line
+         10
+         p
+         note
+         ct-find
+         x1
+         cb-find)))
+     ; Base drawing
+     (vc-append
+      200
+      (vc-append
+       100
+       (object x y)
+       ($ (latex-symbol "nsim"))
+       (object x1 y))
+      note))))
+
+
+(slide
+ inconsistent-example)
+
+(slide
+ #:title "Restriction"
+ (item ($ (string-join (list
+                        (latex '(restrict (type "int")
+                                          (dyn-type)))
+                        (latex '(dyn-type)))
+                       "=")))
+ (item ($ (format "~a = ~a"
+                  (latex '(restrict (type "int")
+                                    (type "bool")))
+                  (latex '(type "int")))))
+ (item ($ (format "~a = ~a"
+                  (latex '(restrict (object
+                                     (method "x"
+                                             (signature (type "int") (type "int")))
+                                     (method "y"
+                                             (signature (type "int") (type "int"))))
+                                    (object
+                                     (method "x"
+                                             (signature (dyn-type) (dyn-type)))
+                                     (method "y"
+                                             (signature (type "int") (type "int"))))))
+                  (latex '(object
+                           (method "x"
+                                   (signature (dyn-type) (dyn-type)))
+                           (method "y"
+                                   (signature (type "int") (type "int")))))
+                  )))
+ )
+
+(slide
+ #:title "Type consistency"
+ 
+ 
+ )
+
+(slide
+ #:title "Properties"
+ (let [(sim ($ (latex-symbol "sim")))]
+   (vl-append
+     (item sim "reflexive")
+     (item sim "symmetric")
+     (item sim "not transitive"
+           (subitem (fast-pict '(consistent (type "int") (dyn-type)))
+                    "and"
+                    (fast-pict '(consistent (type "bool") (dyn-type)))
+                    "but"
+                    (fast-pict '(inconsistent (type "bool") (type "int")))))
+     (item ($ (latex '(consistent (type-var "tau")
+                                  (restrict (type-var "tau")
+                                            (type-var "sigma"))))))
+ )))
+
+
 (define sub-type-table1
- (let* [(tau    (inset (pict '(type-var "tau")) 10))
-        (sigma  (inset (pict '(type-var "sigma")) 10))
-        (sigma1 (inset (pict '(type-var (prime "sigma"))) 10))
+ (let* [(tau    (inset (fast-pict '(type-var "tau")) 10))
+        (sigma  (inset (fast-pict '(type-var "sigma")) 10))
+        (sigma1 (inset (fast-pict '(type-var (prime "sigma"))) 10))
         (combined
          (table 2
                 (list
@@ -151,9 +389,9 @@ latex
    )))
 
 (define sub-type-table2
-  (let* [(tau    (inset (pict '(type-var "tau")) 10))
-         (sigma  (inset (pict '(type-var "sigma")) 10))
-         (sigma2 (inset (pict '(type-var (second "sigma"))) 10))
+  (let* [(tau    (inset (fast-pict '(type-var "tau")) 10))
+         (sigma  (inset (fast-pict '(type-var "sigma")) 10))
+         (sigma2 (inset (fast-pict '(type-var (second "sigma"))) 10))
          (combined
           (table 2
                  (list
@@ -206,143 +444,7 @@ latex
      )))
 
 (slide
- #:title "The idea"
- (item "add a type \"dynamic\":" (pict '(dyn-type)))
- (item "add relations between types and" (pict '(dyn-type))))
-
-(slide
- (item (pict '(consistent (type "int") (type "int"))))
- (item (pict '(consistent (type "int") (dyn-type))))
- (item ($ (latex '(consistent
-                   (fun-type (type "int") (dyn-type))
-                   (fun-type (dyn-type) (type "int"))))))
- )
-
-(slide
- ($ (latex '(consistent
-             (object
-              (method "x"
-                      (signature (type "int") (dyn-type)))
-              (method "y"
-                      (signature (dyn-type) (type "bool"))))
-             (object
-              (method "y"
-                      (signature (type "bool") (dyn-type)))
-              (method "x"
-                      (signature (dyn-type) (type "int"))))))))
-
-(slide
- ($ (latex '(inconsistent
-             (object
-              (method "x"
-                      (signature (type "int") (type "int")))
-              (method "y"
-                      (signature (dyn-type) (dyn-type))))
-             (object
-              (method "x"
-                      (signature (type "int") (type "int"))))))))
-
-
-(define inconsistent-example
-  (let* [(note (t "different types"))
-         (spacing 200)]
-    ((compose
-      (lambda (p)
-        (linewidth 3 p))
-      (lambda (p)
-        (pin-over
-         ; Under
-         p
-         note
-         ct-find
-         (pip-arrow-line
-          -400
-          (* -1 spacing)
-          10)))
-      (lambda (p)
-        (pin-over
-         p
-         note
-         ct-find
-         (pip-arrow-line
-          150
-          (* -1 spacing)
-          10))))
-     ; Base drawing
-     (vc-append
-      200
-      ($ (latex '(inconsistent
-                  (object
-                   (method "x"
-                           (signature (type "int") (type "int")))
-                   (method "y"
-                           (signature (dyn-type) (type "bool"))))
-                  (object
-                   (method "x"
-                           (signature (type "bool") (type "int")))
-                   (method "y"
-                           (signature (dyn-type) (type "bool")))))))
-      note))))
-
-
-(slide
- inconsistent-example)
-
-(slide
- #:title "Restriction"
- (item ($ (string-join (list
-                        (latex '(restrict (type "int")
-                                          (dyn-type)))
-                        (latex '(dyn-type)))
-                       "=")))
- (item ($ (format "~a = ~a"
-                  (latex '(restrict (type "int")
-                                    (type "bool")))
-                  (latex '(type "int")))))
- (item ($ (format "~a = ~a"
-                  (latex '(restrict (object
-                                     (method "x"
-                                             (signature (type "int") (type "int")))
-                                     (method "y"
-                                             (signature (type "int") (type "int"))))
-                                    (object
-                                     (method "x"
-                                             (signature (dyn-type) (dyn-type)))
-                                     (method "y"
-                                             (signature (type "int") (type "int"))))))
-                  (latex '(object
-                           (method "x"
-                                   (signature (dyn-type) (dyn-type)))
-                           (method "y"
-                                   (signature (type "int") (type "int")))))
-                  )))
- )
-
-(slide
- #:title "Type consistency"
- 
- 
- )
-
-(slide
- #:title "Properties"
- (let [(sim ($ (latex-symbol "sim")))]
-   (vl-append
-     (item sim "reflexive")
-     (item sim "symmetric")
-     (item sim "not transitive"
-           (subitem (pict '(consistent (type "int") (dyn-type)))
-                    "and"
-                    (pict '(consistent (type "bool") (dyn-type)))
-                    "but"
-                    (pict '(inconsistent (type "bool") (type "int")))))
-     (item ($ (latex '(consistent (type-var "tau")
-                                  (restrict (type-var "tau")
-                                            (type-var "sigma"))))))
- )))
-
-
-(slide
+ (bt "Consistency and Subtyping")
  (hc-append
   sub-type-table1
   (vline 100 200)
