e2bc4372e9ac166e2eca0ff87176ccf78aef48b8
Noric Couderc <noric.couderc@cs.lth.se>
2022-04-02 18:25:44 +0200

    Changed rule for digit

--- syntax.pl
+++ syntax.pl
@@ -19,7 +19,16 @@ variable(fst) --> "fst".
 variable(snd) --> "snd".
 variable(variable(V)) --> varname(Cs), { atom_chars(V, Cs) }.
 
-digit(N) --> [C], { code_type(C, digit), atom_number(C, N) }.
+digit(0) --> "0".
+digit(1) --> "1".
+digit(2) --> "2".
+digit(3) --> "3".
+digit(4) --> "4".
+digit(5) --> "5".
+digit(6) --> "6".
+digit(7) --> "7".
+digit(8) --> "8".
+digit(9) --> "9".
 
 % Copied from S-Overflow
 nat(N)   --> digit(D), nat(D,N).
